6b1c8fa505ed35d97212bb40f6b2094bce323072
Jenny Fothergill <jennyfothergill@gmail.com>
2021-02-22 15:23:17 -0500

    fix super inits

--- templates/bridges2custom.sh
+++ templates/bridges2custom.sh
@@ -1,5 +1,6 @@
 {% extends "bridges2.sh" %}
 {% block tasks %}
+{{ super() -}}
 #SBATCH --output=workspace/{{operations[0]._jobs[0]}}/job_%j.o
 #SBATCH --error=workspace/{{operations[0]._jobs[0]}}/job_%j.e
 {% endblock tasks %}
