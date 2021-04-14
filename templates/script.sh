8f8ab08de9cbbaf890cbbcd5c131111d87dca941
Jenny <39961845+jennyfothergill@users.noreply.github.com>
2021-04-14 14:42:34 -0600

    switch to 48 hours

--- templates/script.sh
+++ templates/script.sh
@@ -3,5 +3,5 @@
 {{ super() -}}
 # set walltime limit to 24 hours from now (minus 10 minutes)
 # https://hoomd-blue.readthedocs.io/en/v2.9.3/restartable-jobs.html#cleanly-exit-before-the-walltime-limit
-export HOOMD_WALLTIME_STOP=$((`date +%s` + 24 * 3600 - 10 * 60))
+export HOOMD_WALLTIME_STOP=$((`date +%s` + 48 * 3600 - 10 * 60))
 {% endblock project_header %}
