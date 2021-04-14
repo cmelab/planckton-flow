f27a4c72c7cb742c9a8225556fe5f502f2495d1f
Jenny <39961845+jennyfothergill@users.noreply.github.com>
2021-04-14 14:43:58 -0600

    specify max time so bridges doesnt go with default
    default is 1 hour (i think this changed recently)

--- templates/bridges2custom.sh
+++ templates/bridges2custom.sh
@@ -3,6 +3,7 @@
 {{ super() -}}
 #SBATCH --output=workspace/{{operations[0]._jobs[0]}}/job_%j.o
 #SBATCH --error=workspace/{{operations[0]._jobs[0]}}/job_%j.e
+#SBATCH -t 48:00:00
 {% endblock tasks %}
 {% block header %}
 {{ super() -}}
